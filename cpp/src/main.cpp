d74b0a7ce885170eb4fc89fbf7d111e85467b70c
david <david.rohusch@post.cz>
2023-04-21 11:30:03 +0200

    argument check

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,6 +1,13 @@
-#include  "CEmulator.h"
+#include <fmt/core.h>
+// #include  "CEmulator.h"
 
-int main(){
+
+int main(int argc, char *argv[]){
+    // First argument is the name of the ROM file
+    if(argc!= 2){
+        fmt::print("Usage: {} <ROM file>\n", argv[0]);
+        return 1;
+    }
 
     //CEmulator emulator;
 

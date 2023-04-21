3bcc5c82f62d8479b0e22d655116d7c9c129eb06
david <david.rohusch@post.cz>
2023-04-21 12:27:37 +0200

    argument parser

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,15 +1,29 @@
+#include <boost/program_options.hpp>
+#include <cstdio>
 #include <fmt/core.h>
+#include <iostream>
+
 // #include  "CEmulator.h"
+namespace po = boost::program_options;
+int main(int argc, char *argv[]) {
+  // Declare the supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()("help", "produce help message")("rom", po::value<std::string>(),
+                                                     "loads the rom file");
 
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
 
-int main(int argc, char *argv[]){
-    // First argument is the name of the ROM file
-    if(argc!= 2){
-        fmt::print("Usage: {} <ROM file>\n", argv[0]);
-        return 1;
-    }
+  if (vm.count("help")) {
+    desc.print(std::cerr);
+    return 1;
+  }
 
-    //CEmulator emulator;
+  if (!vm.count("rom")) {
+    fmt::print("Missing rom file\n");
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }

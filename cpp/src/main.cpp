8fda1a6512f26dfbb6cf54ccdd0ed671431b5045
david <david.rohusch@post.cz>
2023-04-21 15:46:35 +0200

    Rom location parser added

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,15 +1,15 @@
 #include <boost/program_options.hpp>
 #include <cstdio>
+#include <filesystem>
 #include <fmt/core.h>
 #include <iostream>
 
-// #include  "CEmulator.h"
 namespace po = boost::program_options;
+
 int main(int argc, char *argv[]) {
-  // Declare the supported options.
   po::options_description desc("Allowed options");
-  desc.add_options()("help", "produce help message")("rom", po::value<std::string>(),
-                                                     "loads the rom file");
+  desc.add_options()("help", "produce help message")(
+      "rom", po::value<std::string>(), "loads the rom file");
 
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -22,6 +22,10 @@ int main(int argc, char *argv[]) {
 
   if (!vm.count("rom")) {
     fmt::print("Missing rom file\n");
+    return 1;
+  }
+  if (!std::filesystem::exists(vm["rom"].as<std::string>())) {
+    fmt::print("{} File does not exist\n", vm["rom"].as<std::string>());
     return 1;
   }
 
